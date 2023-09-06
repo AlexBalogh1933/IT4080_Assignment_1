39b9a8f525393cc4c62d529727870f3ff652669d
AlexBalogh1933 <stickmanaloox@gmail.com>
2023-09-06 09:46:18 -0400

    Added Network Setup Part 2

--- Assets/Scripts/NetworkHandler.cs
+++ Assets/Scripts/NetworkHandler.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
-public class NetworkHandler : MonoBehaviour
+public class NetworkHandler : NetworkBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
+        NetworkManager.Singleton.OnClientStarted += OnClientStarted;
+        NetworkManager.Singleton.OnServerStarted += OnServerStarted;
+    }
+
+    private bool hasPrinted = false;
+    private void PrintMe() 
+    { 
+        if (hasPrinted)
+        {
+            return;
+        }
+        Debug.Log("I AM");
+        hasPrinted = true;
+        if (IsServer)
+        {
+            Debug.Log("  the Server!");
+        }
+        if (IsHost)
+        {
+            Debug.Log("  the Host!");
+        }
+        if (IsClient)
+        {
+            Debug.Log("  a Client!");
+        }
+        if (!IsServer && !IsClient)
+        {
+            Debug.Log("  Nothing yet");
+            hasPrinted = false;
+        }
+    }
+
+    private void OnClientStarted()
+    {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnServerStarted()
     {
         
+    }
+
+    private void ServerSetup()
+    {
+
     }
 }
