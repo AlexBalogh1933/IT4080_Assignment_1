aed3a2273a97701eacc697a04d5b871a03b725e6
AlexBalogh1933 <stickmanaloox@gmail.com>
2023-09-06 10:32:51 -0400

    Added Network Setup Part 3, Started Assignment

--- Assets/Scripts/NetworkHandler.cs
+++ Assets/Scripts/NetworkHandler.cs
@@ -8,13 +8,13 @@ public class NetworkHandler : NetworkBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        NetworkManager.Singleton.OnClientStarted += OnClientStarted;
-        NetworkManager.Singleton.OnServerStarted += OnServerStarted;
+        NetworkManager.OnClientStarted += OnClientStarted;
+        NetworkManager.OnServerStarted += OnServerStarted;
     }
 
     private bool hasPrinted = false;
-    private void PrintMe() 
-    { 
+    private void PrintMe()
+    {
         if (hasPrinted)
         {
             return;
@@ -23,15 +23,15 @@ public class NetworkHandler : NetworkBehaviour
         hasPrinted = true;
         if (IsServer)
         {
-            Debug.Log("  the Server!");
+            Debug.Log($"  the Server! {NetworkManager.ServerClientId}");
         }
-        if (IsHost)
+        if (IsHost) 
         {
-            Debug.Log("  the Host!");
+            Debug.Log($"  the Host! {NetworkManager.ServerClientId}/{NetworkManager.LocalClientId}");
         }
         if (IsClient)
         {
-            Debug.Log("  a Client!");
+            Debug.Log($"  a Client! {NetworkManager.LocalClientId}");
         }
         if (!IsServer && !IsClient)
         {
@@ -42,16 +42,67 @@ public class NetworkHandler : NetworkBehaviour
 
     private void OnClientStarted()
     {
-        
+        Debug.Log("!! Client Started !!");
+        NetworkManager.OnClientConnectedCallback += ClientOnClientConnected;
+        NetworkManager.OnClientDisconnectCallback += ClientOnClientDisconnected;
+        NetworkManager.OnClientStopped += ClientOnClientStopped;
+        PrintMe();
     }
 
+    // ---------------------
+    // Client Actions
+    // ---------------------
+    private void ClientOnClientConnected(ulong clientId)
+    {
+        PrintMe();
+        // Print I {clientId} have connected to the server
+        // handle the case when we are the client running on the host.
+        // Some other client connected.
+        Debug.Log($"I client {clientId} has connected to the server");
+    }
+    private void ClientOnClientDisconnected(ulong clientId)
+    {
+        // print I {clientId have disconnected from the server.
+        Debug.Log($"I client {clientId} has disconnected from the server");
+    }
+    private void ClientOnClientStopped(bool indicator)
+    {
+        Debug.Log("!! Client Stopped !!");
+        hasPrinted = false;
+        NetworkManager.OnClientConnectedCallback -= ClientOnClientConnected;
+        NetworkManager.OnClientDisconnectCallback -= ClientOnClientDisconnected;
+        NetworkManager.OnClientStopped -= ClientOnClientStopped;
+    }
+
+
+
+
+    // ---------------------
+    // Server Actions
+    // ---------------------
     private void OnServerStarted()
     {
-        
+        Debug.Log("!! Server Started !!");
+        NetworkManager.OnClientConnectedCallback += ServerOnClientConnected;
+        NetworkManager.OnClientDisconnectCallback += ServerOnClientDisconnected;
+        NetworkManager.OnServerStopped += ServerOnServerStopped;
+        PrintMe();
     }
 
-    private void ServerSetup()
+    private void ServerOnClientConnected(ulong clientId)
     {
-
+        Debug.Log($"Client {clientId} connected to the server");
+    }
+    private void ServerOnClientDisconnected(ulong clientId)
+    {
+        Debug.Log($"Client {clientId} disconnected from the server");
+    }
+    private void ServerOnServerStopped(bool indicator)
+    {
+        Debug.Log("!! Server Stopped !!");
+        hasPrinted = false;
+        NetworkManager.OnClientConnectedCallback -= ServerOnClientConnected;
+        NetworkManager.OnClientDisconnectCallback -= ServerOnClientDisconnected;
+        NetworkManager.OnServerStopped -= ServerOnServerStopped;
     }
 }
