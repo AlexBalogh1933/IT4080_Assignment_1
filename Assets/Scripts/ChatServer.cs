1a2249fa45b8ab99b1a025d67a4c3117900aecf0
AlexBalogh1933 <stickmanaloox@gmail.com>
2023-09-23 19:26:20 -0400

    Added Additional Features

--- Assets/Scripts/ChatServer.cs
+++ Assets/Scripts/ChatServer.cs
@@ -8,6 +8,7 @@ public class ChatServer : NetworkBehaviour
     public ChatUi chatUi;
     const ulong SYSTEM_ID = ulong.MaxValue;
     private ulong[] dmClientIds = new ulong[2];
+    private List<ulong> connectedClients = new List<ulong>();
 
     void Start()
     {
@@ -17,9 +18,11 @@ public class ChatServer : NetworkBehaviour
         if (IsServer)
         {
             NetworkManager.OnClientConnectedCallback += ServerOnClientConnected;
+            NetworkManager.OnClientDisconnectCallback += ServerOnClientDisconnected;
             if (IsHost)
             {
                 DisplayMessageLocally(SYSTEM_ID, $"You are the host AND client {NetworkManager.LocalClientId}");
+                DisplayMessageLocally(SYSTEM_ID, $"Welcome to the server client {NetworkManager.LocalClientId}.");
             }
             else
             {
@@ -29,15 +32,24 @@ public class ChatServer : NetworkBehaviour
         else
         {
             DisplayMessageLocally(SYSTEM_ID, $"You are a client {NetworkManager.LocalClientId}");
+            DisplayMessageLocally(SYSTEM_ID, $"Welcome to the server client {NetworkManager.LocalClientId}.");
         }
     }
 
     private void ServerOnClientConnected(ulong clientId)
     {
-        ServerSendDirectMessage(
-            $"I ({NetworkManager.LocalClientId}) see you ({clientId}) have connected to the server, well done",
-            NetworkManager.LocalClientId,
-            clientId);
+        //ServerSendDirectMessage(
+        //    $"I ({NetworkManager.LocalClientId}) see you ({clientId}) have connected to the server, well done",
+        //    NetworkManager.LocalClientId,
+        //    clientId);
+        SendChatMessageServerRpc($"Client ({clientId}) has connected to the server");
+        connectedClients.Add(clientId);
+
+    }
+    private void ServerOnClientDisconnected(ulong clientId)
+    {
+        SendChatMessageServerRpc($"Client ({clientId}) has disconnected from the server");
+        connectedClients.Remove(clientId);
     }
 
     private void DisplayMessageLocally(ulong from, string message)
@@ -73,7 +85,17 @@ public class ChatServer : NetworkBehaviour
             string clientIdStr = parts[0].Replace("@", "");
             ulong toClientId = ulong.Parse(clientIdStr);
 
-            ServerSendDirectMessage(message, serverRpcParams.Receive.SenderClientId, toClientId);
+            if (connectedClients.Contains(toClientId))
+            {
+                ServerSendDirectMessage(message, serverRpcParams.Receive.SenderClientId, toClientId);
+            }
+            else
+            {
+                ServerSendDirectMessage(
+                    $"Client {toClientId} does not exist. Try again",
+                    SYSTEM_ID,
+                    serverRpcParams.Receive.SenderClientId);
+            }
         }
         else
         {
@@ -108,6 +130,4 @@ public class ChatServer : NetworkBehaviour
         //clientIds[0] = to;
         ReceiveChatMessageClientRpc($"<whisper> {message}", from, rpcParams);
     }
-
-
 }
