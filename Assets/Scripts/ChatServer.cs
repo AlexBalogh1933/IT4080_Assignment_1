81ce4729bbbe45f5fa4b12a7577f39ff807359f9
AlexBalogh1933 <stickmanaloox@gmail.com>
2023-09-23 17:05:18 -0400

    Added Chat Part 2

--- Assets/Scripts/ChatServer.cs
+++ Assets/Scripts/ChatServer.cs
@@ -6,12 +6,47 @@ using Unity.Netcode;
 public class ChatServer : NetworkBehaviour
 {
     public ChatUi chatUi;
-
+    const ulong SYSTEM_ID = ulong.MaxValue;
+    
 
     void Start()
     {
         chatUi.printEnteredText = false;
         chatUi.MessageEntered += OnChatUiMessageEntered;
+
+        if (IsServer)
+        {
+            if (IsHost)
+            {
+                DisplayMessageLocally(SYSTEM_ID, $"You are the host AND client {NetworkManager.LocalClientId}");
+            }
+            else
+            {
+                DisplayMessageLocally(SYSTEM_ID, "You are the server");
+            }
+        }
+        else
+        {
+            DisplayMessageLocally(SYSTEM_ID, $"You are a client {NetworkManager.LocalClientId}");
+        }
+    }
+
+    private void DisplayMessageLocally(ulong from, string message)
+    {
+        string fromStr = $"Player {from}";
+        Color textColor = chatUi.defaultTextColor;
+
+        if (from == NetworkManager.LocalClientId)
+        {
+            fromStr = "you";
+            textColor = Color.magenta;
+        }
+        else if (from == SYSTEM_ID)
+        {
+            fromStr = "SYS";
+            textColor = Color.green;
+        }
+        chatUi.addEntry(fromStr, message, textColor);
     }
 
     private void OnChatUiMessageEntered(string message)
@@ -25,10 +60,9 @@ public class ChatServer : NetworkBehaviour
         ReceiveChatMessageClientRpc(message, serverRpcParams.Receive.SenderClientId);
     }
 
-
     [ClientRpc]
     public void ReceiveChatMessageClientRpc(string message, ulong from, ClientRpcParams clientRpcParams = default)
     {
-        chatUi.addEntry($"Player {from}", message);
+        DisplayMessageLocally(from, message);
     }
 }
